19a4aedcf879c0b02f07a7567e9499a132caafbc
Gaya Blau <gayablau@gmail.com>
2021-11-29 09:28:59 +0200

    profile UI and reminders UI

--- src/RemindersList.tsx
+++ src/RemindersList.tsx
@@ -6,125 +6,138 @@ import { SocketContext } from "./contexts/socket/SocketContext";
 
 
 type ReminderDetailsProps = {
-    handleClose : () => void
+    handleClose: () => void
 
 }
 
 export default function Reminders() {
-    const [modalIsOpen,setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    const setModalIsOpenToTrue =()=>{
+    const setModalIsOpenToTrue = () => {
         setModalIsOpen(true)
     }
 
-    const setModalIsOpenToFalse =()=>{
+    const setModalIsOpenToFalse = () => {
         setModalIsOpen(false)
     }
 
     return (
-        <div className="reminders-list">
-            <Tile>
-                <h2>שלום</h2>
-                <div className="reminders-table">
-                    {modalIsOpen ? <ReminderDetails handleClose={setModalIsOpenToFalse} /> : <h1>hi</h1>}
-                    <RemindersTable />
-                </div>
-                <div className="buttons">
-                    <Button onClick={setModalIsOpenToTrue} primary>הוסף התראה</Button>
-                    <Button primary danger>התנתק</Button>
-                </div>
-            </Tile>
-        </div>
+        <>
+
+            <div className="reminders-list">
+                <Tile>
+                    <h2>שלום</h2>
+                    <div className="reminders-table">
+                        <RemindersTable />
+                    </div>
+                    <div className="buttons">
+                        <Button onClick={setModalIsOpenToTrue} primary>הוסף התראה</Button>
+                        <Button primary danger>התנתק</Button>
+                    </div>
+                </Tile>
+            </div>
+             <div>
+                {modalIsOpen ? <ReminderDetails handleClose={setModalIsOpenToFalse} /> : <div></div> }
+            </div>
+        </>
+
     );
 }
 
 function RemindersTable() {
 
     return (
-    <Table fixed striped sticky>
-        <Table.Head>
-            <Table.Row>
-                <Table.Cell>מחיקה</Table.Cell>
-                <Table.Cell align='center'>תיאור</Table.Cell>
-                <Table.Cell align='center'>שעה</Table.Cell>
-                <Table.Cell align='center'>תאריך</Table.Cell>
-                <Table.Cell align='center'>שם</Table.Cell>
-            </Table.Row>
-        </Table.Head>
-        <Table.Body>
-            <Table.Row>
-                <Table.Cell align='center'>159</Table.Cell>
-                <Table.Cell align='center'>6</Table.Cell>
-                <Table.Cell align='center'>24</Table.Cell>
-                <Table.Cell align='center'>4</Table.Cell>
-                <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell align='center'>159</Table.Cell>
-                <Table.Cell align='center'>6</Table.Cell>
-                <Table.Cell align='center'>24</Table.Cell>
-                <Table.Cell align='center'>4</Table.Cell>
-                <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell align='center'>159</Table.Cell>
-                <Table.Cell align='center'>6</Table.Cell>
-                <Table.Cell align='center'>24</Table.Cell>
-                <Table.Cell align='center'>4</Table.Cell>
-                <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell align='center'>159</Table.Cell>
-                <Table.Cell align='center'>6</Table.Cell>
-                <Table.Cell align='center'>24</Table.Cell>
-                <Table.Cell align='center'>4</Table.Cell>
-                <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell align='center'>237</Table.Cell>
-                <Table.Cell align='center'>9</Table.Cell>
-                <Table.Cell align='center'>37</Table.Cell>
-                <Table.Cell align='center'>4.3</Table.Cell>
-                <Table.Cell is='th' scope='row'>Ice cream sandwich</Table.Cell>
-            </Table.Row>
-        </Table.Body>
-    </Table>)
+        <Table fixed striped sticky className="reminders-table">
+            <Table.Head>
+                <Table.Row>
+                    <Table.Cell>מחיקה</Table.Cell>
+                    <Table.Cell align='center'>תיאור</Table.Cell>
+                    <Table.Cell align='center'>שעה</Table.Cell>
+                    <Table.Cell align='center'>תאריך</Table.Cell>
+                    <Table.Cell align='center'>שם</Table.Cell>
+                </Table.Row>
+            </Table.Head>
+            <Table.Body>
+                <Table.Row>
+                    <Table.Cell align='center'>159</Table.Cell>
+                    <Table.Cell align='center'>6</Table.Cell>
+                    <Table.Cell align='center'>24</Table.Cell>
+                    <Table.Cell align='center'>4</Table.Cell>
+                    <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell align='center'>159</Table.Cell>
+                    <Table.Cell align='center'>6</Table.Cell>
+                    <Table.Cell align='center'>24</Table.Cell>
+                    <Table.Cell align='center'>4</Table.Cell>
+                    <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell align='center'>159</Table.Cell>
+                    <Table.Cell align='center'>6</Table.Cell>
+                    <Table.Cell align='center'>24</Table.Cell>
+                    <Table.Cell align='center'>4</Table.Cell>
+                    <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell align='center'>159</Table.Cell>
+                    <Table.Cell align='center'>6</Table.Cell>
+                    <Table.Cell align='center'>24</Table.Cell>
+                    <Table.Cell align='center'>4</Table.Cell>
+                    <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell align='center'>159</Table.Cell>
+                    <Table.Cell align='center'>6</Table.Cell>
+                    <Table.Cell align='center'>24</Table.Cell>
+                    <Table.Cell align='center'>4</Table.Cell>
+                    <Table.Cell is='th' scope='row'>Frozen yoghurt</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell align='center'>237</Table.Cell>
+                    <Table.Cell align='center'>9</Table.Cell>
+                    <Table.Cell align='center'>37</Table.Cell>
+                    <Table.Cell align='center'>4.3</Table.Cell>
+                    <Table.Cell is='th' scope='row'>Ice cream sandwich</Table.Cell>
+                </Table.Row>
+            </Table.Body>
+        </Table>)
 }
 
-function ReminderDetails(props : ReminderDetailsProps) {
-    
+function ReminderDetails(props: ReminderDetailsProps) {
+
     return (
-        <div>
-             <Modal>
-            <div className="reminder-details">
-                <Modal.Header>
-                    <Modal.Title>הוסף התראה</Modal.Title>
-                    <Modal.Close onClick={props.handleClose}/>
-                </Modal.Header>
-                <Modal.Content>
-                    <form className="reminder-details-form">
-                        <div className="input">
-                            <InputBox className="input-box" placeholder='שם' type='text' />
-                        </div>
-                        <div className="input">
-                            <InputBox className="input-box" placeholder='תיאור' type='password' />
-                        </div>
-                        <div className="input">
-                            <InputBox className="input-box" placeholder='תאריך' type='password' />
-                        </div>
-                        <div className="input">
-                            <InputBox className="input-box" placeholder='שעה' type='password' />
-                        </div>
-                    </form>
-                </Modal.Content>
-                <Modal.Footer>
-                    <Button primary>הוסף</Button>
-                </Modal.Footer>
-            </div>
-        </Modal>
+        <div className="reminder-details-modal">
+            <Modal>
+                <div className="reminder-details">
+                    <Modal.Header>
+                        <Modal.Title>הוסף התראה</Modal.Title>
+                        <Modal.Close onClick={props.handleClose} />
+                    </Modal.Header>
+                    <Modal.Content>
+                        <form className="reminder-details-form">
+                            <div className="input">
+                                <InputBox className="input-box" placeholder='שם' type='text' />
+                            </div>
+                            <div className="input">
+                                <InputBox className="input-box" placeholder='תיאור' type='password' />
+                            </div>
+                            <div className="input">
+                                <InputBox className="input-box" placeholder='תאריך' type='password' />
+                            </div>
+                            <div className="input">
+                                <InputBox className="input-box" placeholder='שעה' type='password' />
+                            </div>
+                        </form>
+                    </Modal.Content>
+                    <Modal.Footer>
+                        <Button primary>הוסף</Button>
+                    </Modal.Footer>
+                </div>
+            </Modal>
         </div>
 
-       
+
     )
 }
 
